b8548de755c97ffafe48610faba1d7d2d827242c
avdyachenko <alex_d94@mail.ru>
2021-05-10 13:38:48 +0500

    AKARI added with sigma_clipped background estimation

--- AKARI_photometry.py
+++ AKARI_photometry.py
@@ -12,123 +12,124 @@ from photutils import SkyCircularAperture
 from photutils import CircularAperture
 from photutils import CircularAnnulus
 from photutils import aperture_photometry
+from astropy.stats import sigma_clipped_stats
 
-def wise1(image_data, radpix, wcs):
+def photometry_AKARI_N60 (image_data, radpix, wcs):
     #aperture = CircularAperture((image_data.shape[0]/2,image_data.shape[0]/2), radpix[19])
     position = [image_data.shape[0]/2,image_data.shape[0]/2]
     apertures = [CircularAperture(position, r=r) for r in radpix]
     annulus_aperture = CircularAnnulus(position, r_in=radpix[19], r_out=radpix[19]+radpix[16])
     phot_table = aperture_photometry(image_data, apertures, wcs=wcs)
-    bkg_table = aperture_photometry(image_data, annulus_aperture, wcs=wcs)
-    bkg_mean = bkg_table['aperture_sum'] / annulus_aperture.area()
+
+    annulus_masks = annulus_aperture.to_mask(method='center')
+    annulus_data = annulus_masks.multiply(image_data)
+    mask = annulus_masks.data
+    annulus_data_1d = annulus_data[mask > 0]
+    _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
+    bkg_mean = median_sigclip
+
     #sky_aperture = to_sky(apertures,wcs)
     phot_array = np.zeros(20)
     bkg_sum_array = np.zeros(20)
     for i in range(0,20):
         phot_array[i] = phot_table['aperture_sum_' + str(i)][0]
-        bkg_sum_array[i] = bkg_mean * apertures[i].area()
-        phot_table['aperture_sum_' + str(i)].info.format = '%.8g'  # for consistent table output
+        bkg_sum_array[i] = bkg_mean * apertures[i].area
 
     final_sum =  phot_array - bkg_sum_array
 
-    wisemags = np.array([final_sum, 0, 0, 0])
-    coeffs_interp = get_coeffs(*wisemags)
-    f1, f2, f3, f4 = Fsnu0/coeffs_interp * 10**(-wisemags/2.5)
-    print ('Corrected WISE fluxes: %g, %g, %g, %g'%(f1, f2, f3, f4)
-    phot_Jy_wise1 = f1
+    scale = np.mean(proj_plane_pixel_scales(wcs))
+    phot_Jy_AKARI_N60 = final_sum * (scale*np.pi/180.0)**2 # Jy/sr to Jy scale**2/((180/np.pi)**2
 
-    bkg_table ['aperture_sum'].info.format = '%.8g'  # for consistent table output
     print('Backgorud outer radius =',radpix[19]+radpix[16],'pixels')
-    print(bkg_table)
     print(phot_table)
-    return(phot_Jy_wise1)
+    return(phot_Jy_AKARI_N60)
 
-def wise2(image_data, radpix, wcs):
+def photometry_AKARI_WideS(image_data, radpix, wcs):
     #aperture = CircularAperture((image_data.shape[0]/2,image_data.shape[0]/2), radpix[19])
     position = [image_data.shape[0]/2,image_data.shape[0]/2]
     apertures = [CircularAperture(position, r=r) for r in radpix]
     annulus_aperture = CircularAnnulus(position, r_in=radpix[19], r_out=radpix[19]+radpix[16])
     phot_table = aperture_photometry(image_data, apertures, wcs=wcs)
-    bkg_table = aperture_photometry(image_data, annulus_aperture, wcs=wcs)
-    bkg_mean = bkg_table['aperture_sum'] / annulus_aperture.area()
+
+    annulus_masks = annulus_aperture.to_mask(method='center')
+    annulus_data = annulus_masks.multiply(image_data)
+    mask = annulus_masks.data
+    annulus_data_1d = annulus_data[mask > 0]
+    _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
+    bkg_mean = median_sigclip
+
     #sky_aperture = to_sky(apertures,wcs)
     phot_array = np.zeros(20)
     bkg_sum_array = np.zeros(20)
     for i in range(0,20):
         phot_array[i] = phot_table['aperture_sum_' + str(i)][0]
-        bkg_sum_array[i] = bkg_mean * apertures[i].area()
-        phot_table['aperture_sum_' + str(i)].info.format = '%.8g'  # for consistent table output
+        bkg_sum_array[i] = bkg_mean * apertures[i].area
 
     final_sum =  phot_array - bkg_sum_array
 
-    wisemags = np.array([0, final_sum, 0, 0])
-    coeffs_interp = get_coeffs(*wisemags)
-    f1, f2, f3, f4 = Fsnu0/coeffs_interp * 10**(-wisemags/2.5)
-    print ('Corrected WISE fluxes: %g, %g, %g, %g'%(f1, f2, f3, f4)
-    phot_Jy_wise1 = f2
+    scale = np.mean(proj_plane_pixel_scales(wcs))
+    phot_Jy_AKARI_WideS = final_sum * (scale*np.pi/180.0)**2  #
 
-    bkg_table ['aperture_sum'].info.format = '%.8g'  # for consistent table output
+
     print('Backgorud outer radius =',radpix[19]+radpix[16],'pixels')
-    print(bkg_table)
     print(phot_table)
-    return(phot_Jy_wise2)
+    return(phot_Jy_AKARI_WideS)
 
-def wise3(image_data, radpix, wcs):
+def photometry_AKARI_WideL(image_data, radpix, wcs):
     #aperture = CircularAperture((image_data.shape[0]/2,image_data.shape[0]/2), radpix[19])
     position = [image_data.shape[0]/2,image_data.shape[0]/2]
     apertures = [CircularAperture(position, r=r) for r in radpix]
     annulus_aperture = CircularAnnulus(position, r_in=radpix[19], r_out=radpix[19]+radpix[16])
     phot_table = aperture_photometry(image_data, apertures, wcs=wcs)
-    bkg_table = aperture_photometry(image_data, annulus_aperture, wcs=wcs)
-    bkg_mean = bkg_table['aperture_sum'] / annulus_aperture.area()
+    annulus_masks = annulus_aperture.to_mask(method='center')
+    annulus_data = annulus_masks.multiply(image_data)
+    mask = annulus_masks.data
+    annulus_data_1d = annulus_data[mask > 0]
+    _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
+    bkg_mean = median_sigclip
+
     #sky_aperture = to_sky(apertures,wcs)
     phot_array = np.zeros(20)
     bkg_sum_array = np.zeros(20)
     for i in range(0,20):
         phot_array[i] = phot_table['aperture_sum_' + str(i)][0]
-        bkg_sum_array[i] = bkg_mean * apertures[i].area()
-        phot_table['aperture_sum_' + str(i)].info.format = '%.8g'  # for consistent table output
+        bkg_sum_array[i] = bkg_mean * apertures[i].area
 
     final_sum =  phot_array - bkg_sum_array
 
-    wisemags = np.array([0, 0, final_sum, 0])
-    coeffs_interp = get_coeffs(*wisemags)
-    f1, f2, f3, f4 = Fsnu0/coeffs_interp * 10**(-wisemags/2.5)
-    print ('Corrected WISE fluxes: %g, %g, %g, %g'%(f1, f2, f3, f4)
-    phot_Jy_wise1 = f3
+    scale = np.mean(proj_plane_pixel_scales(wcs))
+    phot_Jy_AKARI_WideS = final_sum * (scale*np.pi/180.0)**2
 
-    bkg_table ['aperture_sum'].info.format = '%.8g'  # for consistent table output
     print('Backgorud outer radius =',radpix[19]+radpix[16],'pixels')
-    print(bkg_table)
     print(phot_table)
-    return(phot_Jy_wise3)
+    return(phot_Jy_AKARI_WideL)
 
-def wise4(image_data, radpix, wcs):
+
+def photometry_AKARI_N160(image_data, radpix, wcs):
     #aperture = CircularAperture((image_data.shape[0]/2,image_data.shape[0]/2), radpix[19])
     position = [image_data.shape[0]/2,image_data.shape[0]/2]
     apertures = [CircularAperture(position, r=r) for r in radpix]
     annulus_aperture = CircularAnnulus(position, r_in=radpix[19], r_out=radpix[19]+radpix[16])
     phot_table = aperture_photometry(image_data, apertures, wcs=wcs)
-    bkg_table = aperture_photometry(image_data, annulus_aperture, wcs=wcs)
-    bkg_mean = bkg_table['aperture_sum'] / annulus_aperture.area()
+    annulus_masks = annulus_aperture.to_mask(method='center')
+    annulus_data = annulus_masks.multiply(image_data)
+    mask = annulus_masks.data
+    annulus_data_1d = annulus_data[mask > 0]
+    _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
+    bkg_mean = median_sigclip
+
     #sky_aperture = to_sky(apertures,wcs)
     phot_array = np.zeros(20)
     bkg_sum_array = np.zeros(20)
     for i in range(0,20):
         phot_array[i] = phot_table['aperture_sum_' + str(i)][0]
-        bkg_sum_array[i] = bkg_mean * apertures[i].area()
-        phot_table['aperture_sum_' + str(i)].info.format = '%.8g'  # for consistent table output
+        bkg_sum_array[i] = bkg_mean * apertures[i].area
 
     final_sum =  phot_array - bkg_sum_array
 
-    wisemags = np.array([0, 0, 0, final_sum])
-    coeffs_interp = get_coeffs(*wisemags)
-    f1, f2, f3, f4 = Fsnu0/coeffs_interp * 10**(-wisemags/2.5)
-    print ('Corrected WISE fluxes: %g, %g, %g, %g'%(f1, f2, f3, f4)
-    phot_Jy_wise1 = f4
+    scale = np.mean(proj_plane_pixel_scales(wcs))
+    phot_Jy_AKARI_N160 = final_sum * (scale*np.pi/180.0)**2
 
-    bkg_table ['aperture_sum'].info.format = '%.8g'  # for consistent table output
     print('Backgorud outer radius =',radpix[19]+radpix[16],'pixels')
-    print(bkg_table)
     print(phot_table)
-    return(phot_Jy_wise4)
+    return(phot_Jy_AKARI_N160)
--- MSX_photometry.py
+++ MSX_photometry.py
@@ -1,6 +1,3 @@
-
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,6 +12,7 @@ from photutils import SkyCircularAperture
 from photutils import CircularAperture
 from photutils import CircularAnnulus
 from photutils import aperture_photometry
+from astropy.stats import sigma_clipped_stats
 
 def photometry_MSX_A(image_data, radpix, wcs):
     #aperture = CircularAperture((image_data.shape[0]/2,image_data.shape[0]/2), radpix[19])
@@ -22,25 +20,27 @@ def photometry_MSX_A(image_data, radpix, wcs):
     apertures = [CircularAperture(position, r=r) for r in radpix]
     annulus_aperture = CircularAnnulus(position, r_in=radpix[19], r_out=radpix[19]+radpix[16])
     phot_table = aperture_photometry(image_data, apertures, wcs=wcs)
-    bkg_table = aperture_photometry(image_data, annulus_aperture, wcs=wcs)   
-    bkg_mean = bkg_table['aperture_sum'] / annulus_aperture.area
+
+    annulus_masks = annulus_aperture.to_mask(method='center')
+    annulus_data = annulus_masks.multiply(image_data)
+    mask = annulus_masks.data
+    annulus_data_1d = annulus_data[mask > 0]
+    _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
+    bkg_mean = median_sigclip
+
     #sky_aperture = to_sky(apertures,wcs)
     phot_array = np.zeros(20)
     bkg_sum_array = np.zeros(20)
     for i in range(0,20):
         phot_array[i] = phot_table['aperture_sum_' + str(i)][0]
         bkg_sum_array[i] = bkg_mean * apertures[i].area
-        phot_table['aperture_sum_' + str(i)].info.format = '%.8g'  # for consistent table output
-    
-    final_sum =  phot_array - bkg_sum_array  
-    
-    scale = np.mean(proj_plane_pixel_scales(wcs))    
+
+    final_sum =  phot_array - bkg_sum_array
+
+    scale = np.mean(proj_plane_pixel_scales(wcs))
     phot_Jy_MSX_A = final_sum* 7.133e12 * (scale*np.pi/180.0)**2 # MSX_A W/m^2-sr to Jy *7.133e12*scale**2/((180/np.pi)**2*3600**2)
-    
-    
-    bkg_table ['aperture_sum'].info.format = '%.8g'  # for consistent table output
+
     print('Backgorud outer radius =',radpix[19]+radpix[16],'pixels')
-    print(bkg_table)
     print(phot_table)
     return(phot_Jy_MSX_A)
 
@@ -50,25 +50,28 @@ def photometry_MSX_C(image_data, radpix, wcs):
     apertures = [CircularAperture(position, r=r) for r in radpix]
     annulus_aperture = CircularAnnulus(position, r_in=radpix[19], r_out=radpix[19]+radpix[16])
     phot_table = aperture_photometry(image_data, apertures, wcs=wcs)
-    bkg_table = aperture_photometry(image_data, annulus_aperture, wcs=wcs)   
-    bkg_mean = bkg_table['aperture_sum'] / annulus_aperture.area
+
+    annulus_masks = annulus_aperture.to_mask(method='center')
+    annulus_data = annulus_masks.multiply(image_data)
+    mask = annulus_masks.data
+    annulus_data_1d = annulus_data[mask > 0]
+    _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
+    bkg_mean = median_sigclip
+
     #sky_aperture = to_sky(apertures,wcs)
     phot_array = np.zeros(20)
     bkg_sum_array = np.zeros(20)
     for i in range(0,20):
         phot_array[i] = phot_table['aperture_sum_' + str(i)][0]
         bkg_sum_array[i] = bkg_mean * apertures[i].area
-        phot_table['aperture_sum_' + str(i)].info.format = '%.8g'  # for consistent table output
-    
-    final_sum =  phot_array - bkg_sum_array   
-    
-    scale = np.mean(proj_plane_pixel_scales(wcs))  
+
+    final_sum =  phot_array - bkg_sum_array
+
+    scale = np.mean(proj_plane_pixel_scales(wcs))
     phot_Jy_MSX_C = final_sum* 2.863e13 * (scale*np.pi/180.0)**2  # MSX_C W/m^2-sr to Jy *2.863e13*scale**2/((180/np.pi)**2*3600**2)
 
-    
-    bkg_table ['aperture_sum'].info.format = '%.8g'  # for consistent table output
+
     print('Backgorud outer radius =',radpix[19]+radpix[16],'pixels')
-    print(bkg_table)
     print(phot_table)
     return(phot_Jy_MSX_C)
 
@@ -78,24 +81,26 @@ def photometry_MSX_D(image_data, radpix, wcs):
     apertures = [CircularAperture(position, r=r) for r in radpix]
     annulus_aperture = CircularAnnulus(position, r_in=radpix[19], r_out=radpix[19]+radpix[16])
     phot_table = aperture_photometry(image_data, apertures, wcs=wcs)
-    bkg_table = aperture_photometry(image_data, annulus_aperture, wcs=wcs)   
-    bkg_mean = bkg_table['aperture_sum'] / annulus_aperture.area
+    annulus_masks = annulus_aperture.to_mask(method='center')
+    annulus_data = annulus_masks.multiply(image_data)
+    mask = annulus_masks.data
+    annulus_data_1d = annulus_data[mask > 0]
+    _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
+    bkg_mean = median_sigclip
+
     #sky_aperture = to_sky(apertures,wcs)
     phot_array = np.zeros(20)
     bkg_sum_array = np.zeros(20)
     for i in range(0,20):
         phot_array[i] = phot_table['aperture_sum_' + str(i)][0]
         bkg_sum_array[i] = bkg_mean * apertures[i].area
-        phot_table['aperture_sum_' + str(i)].info.format = '%.8g'  # for consistent table output
-    
-    final_sum =  phot_array - bkg_sum_array   
-    
-    scale = np.mean(proj_plane_pixel_scales(wcs))  
+
+    final_sum =  phot_array - bkg_sum_array
+
+    scale = np.mean(proj_plane_pixel_scales(wcs))
     phot_Jy_MSX_D = final_sum* 3.216e13 * (scale*np.pi/180.0)**2 # MSX_D W/m^2-sr to Jy *3.216e13*scale**2/((180/np.pi)**2*3600**2)
-    
-    bkg_table ['aperture_sum'].info.format = '%.8g'  # for consistent table output
+
     print('Backgorud outer radius =',radpix[19]+radpix[16],'pixels')
-    print(bkg_table)
     print(phot_table)
     return(phot_Jy_MSX_D)
 
@@ -106,23 +111,25 @@ def photometry_MSX_E(image_data, radpix, wcs):
     apertures = [CircularAperture(position, r=r) for r in radpix]
     annulus_aperture = CircularAnnulus(position, r_in=radpix[19], r_out=radpix[19]+radpix[16])
     phot_table = aperture_photometry(image_data, apertures, wcs=wcs)
-    bkg_table = aperture_photometry(image_data, annulus_aperture, wcs=wcs)   
-    bkg_mean = bkg_table['aperture_sum'] / annulus_aperture.area
+    annulus_masks = annulus_aperture.to_mask(method='center')
+    annulus_data = annulus_masks.multiply(image_data)
+    mask = annulus_masks.data
+    annulus_data_1d = annulus_data[mask > 0]
+    _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
+    bkg_mean = median_sigclip
+
     #sky_aperture = to_sky(apertures,wcs)
     phot_array = np.zeros(20)
     bkg_sum_array = np.zeros(20)
     for i in range(0,20):
         phot_array[i] = phot_table['aperture_sum_' + str(i)][0]
         bkg_sum_array[i] = bkg_mean * apertures[i].area
-        phot_table['aperture_sum_' + str(i)].info.format = '%.8g'  # for consistent table output
-    
-    final_sum =  phot_array - bkg_sum_array   
-    
-    scale = np.mean(proj_plane_pixel_scales(wcs))  
+
+    final_sum =  phot_array - bkg_sum_array
+
+    scale = np.mean(proj_plane_pixel_scales(wcs))
     phot_Jy_MSX_E = final_sum * 2.476e13  * (scale*np.pi/180.0)**2 # MSX_E W/m^2-sr to Jy *2.476e13*scale**2/((180/np.pi)**2*3600**2)
-    
-    bkg_table ['aperture_sum'].info.format = '%.8g'  # for consistent table output
+
     print('Backgorud outer radius =',radpix[19]+radpix[16],'pixels')
-    print(bkg_table)
     print(phot_table)
     return(phot_Jy_MSX_E)
